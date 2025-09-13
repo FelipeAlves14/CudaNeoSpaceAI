39398c195cd8b07581f0bcb04e8ad84b9b5d2657
FelipeAlves14 <favasconcelos09@gmail.com>
2025-09-12 22:48:25 -0300

    :bug: fix: corrige casos de teste com N maior que 32

--- junho/aquecimento/aquecimento.cu
+++ junho/aquecimento/aquecimento.cu
@@ -5,42 +5,45 @@
 
 __global__ void calculaCelulas(float *matrizA, float *matrizB, float *matrizC, int N)
 {
-    int x = blockIdx.x * blockDim.x + threadIdx.x;
-    int y = blockIdx.y * blockDim.y + threadIdx.y;
-    float soma = 0.0f;
-    for (int i = 0; i < N; i++)
-        soma += matrizA[x * N + i] * matrizB[i * N + y];
-    matrizC[x * N + y] = soma;
+    int linha = blockIdx.y * blockDim.y + threadIdx.y;
+    int coluna = blockIdx.x * blockDim.x + threadIdx.x;
+    if (linha < N && coluna < N)
+    {
+        float soma = 0.0f;
+        for (int i = 0; i < N; i++)
+            soma += matrizA[linha * N + i] * matrizB[i * N + coluna];
+        matrizC[linha * N + coluna] = soma;
+    }
 }
 
 int main()
 {
     int N;
     std::cin >> N;
-    float *matrizA = new float[N * N];
-    float *matrizB = new float[N * N];
-    float *matrizC = new float[N * N];
-
-    cudaMalloc(&matrizA, N * N * sizeof(float));
-    cudaMalloc(&matrizB, N * N * sizeof(float));
-    cudaMalloc(&matrizC, N * N * sizeof(float));
-    
-    dim3 threadsPerBlock(N, N);
+    int lengthArray = N * N;
+    float *matrizA, *matrizB, *matrizC;
+    cudaMalloc(&matrizA, lengthArray * sizeof(float));
+    cudaMalloc(&matrizB, lengthArray * sizeof(float));
+    cudaMalloc(&matrizC, lengthArray * sizeof(float));
+    int threads = N > 32 ? 32 : N;
+    int blocks = (N + threads - 1) / threads;
+    dim3 threadsPerBlock(threads, threads);
+    dim3 numBlocks(blocks, blocks);
 
     float valor;
-    for (int i = 0; i < N * N; i++)
+    for (int i = 0; i < lengthArray; i++)
     {
         std::cin >> valor;
         cudaMemcpy(&matrizA[i], &valor, sizeof(float), cudaMemcpyHostToDevice);
     }
-    for (int i = 0; i < N * N; i++)
+    for (int i = 0; i < lengthArray; i++)
     {
         std::cin >> valor;
         cudaMemcpy(&matrizB[i], &valor, sizeof(float), cudaMemcpyHostToDevice);
     }
 
-    calculaCelulas<<<1, threadsPerBlock>>>(matrizA, matrizB, matrizC, N);
-    for (int i = 0; i < N * N; i++)
+    calculaCelulas<<<numBlocks, threadsPerBlock>>>(matrizA, matrizB, matrizC, N);
+    for (int i = 0; i < lengthArray; i++)
     {
         cudaMemcpy(&valor, &matrizC[i], sizeof(float), cudaMemcpyDeviceToHost);
         std::cout << std::fixed << std::setprecision(2) << valor << " ";
